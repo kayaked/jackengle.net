89a1dede4c01ce6b1dc3716e3bad5c5eca7e6964
Jack Engle (kayaked) <jack@Jack>
2024-04-21 17:44:22 -0400

    blog use full md folder path

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,6 +1,6 @@
 export async function load({ params }) {
     /* @vite-ignore */
-    const post = await import(`./${params.slug}.md`);
+    const post = await import(`/src/routes/blog/[slug]/${params.slug}.md`);
 
     return {
         content: post.default,
