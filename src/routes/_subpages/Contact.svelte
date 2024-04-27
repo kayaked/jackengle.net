5f0de317771b50b1aacf52e2c2bf7d24088ea488
Jack Engle (kayaked) <jack@Jack>
2024-04-26 21:23:41 -0400

    header lightmode + small css mods

--- src/routes/_subpages/Contact.svelte
+++ src/routes/_subpages/Contact.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <section>
-	<h1 class="mx-8 mb-2 text-4xl font-semibold block md:hidden line-height-0">Contact Me</h1>
+	<h1 class="mx-8 mb-2 text-4xl font-semibold block md:hidden text-black/90 dark:text-white">Contact Me</h1>
 	<div class="mx-6 grid grid-cols-1 md:grid-cols-3 gap-5">
 		<div class=" py-2 md:p-0 md:col-span-2">
 			<div class="p-4 bg-black/25 dark:bg-black/25 rounded-lg dark:border dark:border-neutral-700/30">
@@ -33,8 +33,8 @@
 						<input required type="text" name="name" class="inline-block bg-white/50 dark:bg-white/5 p-3 rounded-lg text-black dark:text-white placeholder:text-black/60 dark:placeholder:text-white/75" placeholder="Name">
 						<input required type="email" name="email" class="inline-block bg-white/50 dark:bg-white/5 p-3 rounded-lg text-black dark:text-white placeholder:text-black/60 dark:placeholder:text-white/75" placeholder="E-mail">
 					</div>
-					<textarea required name="message" class="w-full mt-4 bg-white/50 dark:bg-white/5 min-h-[15rem] rounded-lg p-3 text-black dark:text-white placeholder:text-black/60 dark:placeholder:text-white/75" placeholder="Message (Body)"></textarea>
-				<input type="submit" class="cursor-pointer p-5 mt-3 w-full rounded-lg bg-neutral-900/50 text-white/80 text-lg font-medium dark:hover:text-white/90 hover:bg-black/90 hover:text-white" value="Send Message" />
+					<textarea required name="message" class="w-full mt-4 bg-white/50 dark:bg-white/5 min-h-[8rem] md:min-h-[15rem] rounded-lg p-3 text-black dark:text-white placeholder:text-black/60 dark:placeholder:text-white/75" placeholder="Message (Body)"></textarea>
+				<input type="submit" class="cursor-pointer p-3 mt-3 w-full rounded-lg bg-neutral-900/50 text-white/80 text-lg font-medium dark:hover:text-white/90 hover:bg-black/90 hover:text-white" value="Send Message" />
 				</form>
 			</div>
 			<div class="text-center w-full my-3 text-black/30 dark:text-white/30 text-sm">
@@ -47,7 +47,7 @@
 				{/if}
 			</div>
 		</div>
-		<span class="text-black/80 dark:text-white/80 leading-7 mx-4">
+		<span class="text-black/80 dark:text-white/80 leading-7 mx-2">
 			<p class="mb-2">Reach out by leaving a message!</p>
 			<p>Alternatively, you can email me at 
 				<a class="underline underline-offset-4 decoration-1 text-black/60 hover:text-black/75 dark:text-white/60 dark:hover:text-white/50" href="mailto:jackengle25@gmail.com">jackengle25@gmail.com</a>, connect with me on 
--- src/routes/_subpages/Skills.svelte
+++ src/routes/_subpages/Skills.svelte
@@ -173,8 +173,8 @@
 </script>
 
 <section class="">
-	<h1 class="mx-8 mb-4 text-4xl font-semibold block md:hidden">Technologies</h1>
-    <div class="mx-8">
+	<h1 class="mx-8 mb-4 text-4xl font-semibold block md:hidden text-black/90 dark:text-white">Technologies</h1>
+    <div class="mx-5 md:mx-8">
     {#each techList as section}
         <span class="font-semibold text-xl text-black/90 dark:text-white/90">{section.section}</span>
         <br>
